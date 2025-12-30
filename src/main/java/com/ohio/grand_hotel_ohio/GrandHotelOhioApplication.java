d61016638db793db688dd08ea38d89ee1c8de131
SimonBoldi12 <simiboldi06@gmail.com>
2025-12-30 19:04:54 +0100

    project init

---
+++ src/main/java/com/ohio/grand_hotel_ohio/GrandHotelOhioApplication.java
@@ -0,0 +1,13 @@
+package com.ohio.grand_hotel_ohio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandHotelOhioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandHotelOhioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohio/grand_hotel_ohio/GrandHotelOhioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohio.grand_hotel_ohio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandHotelOhioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
