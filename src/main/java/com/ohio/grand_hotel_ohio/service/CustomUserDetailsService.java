44be85738e3327f601934fb09798eaba675abf5d
vxnczee <21d_vinczea@nyirszikszi.hu>
2026-01-27 18:05:22 +0100

    Add JWT authentication and security configuration

---
+++ src/main/java/com/ohio/grand_hotel_ohio/service/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.ohio.grand_hotel_ohio.service;
+
+import com.ohio.grand_hotel_ohio.exception.OurException;
+import com.ohio.grand_hotel_ohio.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return (UserDetails) userRepository.findByEmail(username).orElseThrow(()-> new OurException("Username/Email not found"));
+    }
+}
---
+++ src/main/java/com/ohio/grand_hotel_ohio/service/utils/JWTUtils.java
@@ -0,0 +1,58 @@
+package com.ohio.grand_hotel_ohio.service.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JWTUtils {
+
+    private static final long EXPIRATION_TIME = 1000L * 60 * 60 * 24 * 7; // 7 nap
+
+    private final SecretKey key;
+
+
+    public JWTUtils(@Value("${jwt.secret}") String secretString) {
+        this.key = Keys.hmacShaKeyFor(secretString.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimsTFunction) {
+        return claimsTFunction.apply(
+                Jwts.parser()
+                        .verifyWith(key)
+                        .build()
+                        .parseSignedClaims(token)
+                        .getPayload()
+        );
+    }
+
+    public boolean isValidToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token, Claims::getExpiration).before(new Date());
+    }
+}
